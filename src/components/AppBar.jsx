1c612275dbbab0bac41337cda6f3317ec9e7f98b
Mike Vautour <mike@intergalacticlab.com>
2023-05-16 12:15:11 -0400

    10.7 horizontal scrollview

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,21 +2,33 @@ import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
+import { ScrollView } from 'react-native';
+
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.bgDark,
     padding: Constants.statusBarHeight,
     paddingTop: "20%",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab
-        label="Repositories"
-      />
+      <ScrollView horizontal={true}>
+        <AppBarTab
+          label="Repositories"
+          link="/"
+        />
+        <AppBarTab
+          label="Sign in"
+          link="/signin"
+        />
+      </ScrollView>
+     
     </View>
   )
 }
--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,20 +1,25 @@
 import Text from './Text'
 import { Pressable, StyleSheet } from 'react-native'
 import theme from '../theme'
+import { Link } from 'react-router-native'
 
 const styles = StyleSheet.create({
   text: {
     fontSize: "24px",
     color: theme.colors.appBarText,
+    paddingRight: 20,
   }  
 })
 
-const AppBarTab = ({label}) => {
+const AppBarTab = ({ label, linkString }) => {
+  console.log(label)
   return (
     <Pressable>
-      <Text style={styles.text}>
-        {label}
-      </Text>
+      <Link to={linkString} >
+        <Text style={styles.text}>
+         {label}
+        </Text>
+      </Link>
     </Pressable>
   )
 }
