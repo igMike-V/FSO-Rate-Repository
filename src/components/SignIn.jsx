d9b33116339612ff50e60f7a6f9d0edee3d75899
Mike Vautour <mike@intergalacticlab.com>
2023-05-23 11:53:15 -0400

    set up authStorage

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,6 +5,7 @@ import Text from './Text';
 import theme from '../theme';
 import * as yup from 'yup';
 import useSignIn from '../hooks/useSignIn';
+import AuthStorage from '../utils/authStorage';
 
 const styles = StyleSheet.create({
   container: {
@@ -58,11 +59,13 @@ const SignInForm = ({ onSubmit }) => {
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  const authStorage = new AuthStorage()
   const onSubmit = async (values) => {
     const { username, password } = values;
     try {
       const response = await signIn({ username, password });
       const token = response.data.authenticate.accessToken
+      authStorage.setAccessToken(token)
       console.log(token)
     } catch (e) {
       console.log(e)
