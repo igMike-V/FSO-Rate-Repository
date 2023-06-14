ae67109072995d146bcd945bd0883d427122d27e
Mike Vautour <mike@iglab.dev>
2023-06-14 15:23:17 -0400

    hook up view repository button

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -1,4 +1,5 @@
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList, Pressable } from "react-native";
+import { useNavigate } from "react-router-native";
 import useMe from "../hooks/useMe";
 import Text from "./Text";
 import Review from "./Review";
@@ -13,9 +14,67 @@ const styles = StyleSheet.create({
     height: 10,
     backgroundColor: theme.colors.bgLight,
   },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexGrow: 1,
+    flexShrink: 1,
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: theme.colors.lightText,
+    fontSize: theme.fontSizes.body,
+    fontWeight: theme.fontWeights.bold,
+  },
+  buttonLeft: {
+    backgroundColor: theme.colors.primary,
+    padding: 30,
+    borderRadius: theme.borders.buttonRadius,
+    marginTop: 20,
+    flexGrow: 1,
+    flexShrink: 1,
+    marginRight: 10,
+  },
+  buttonRight: {
+    backgroundColor: theme.colors.error,
+    padding: 30,
+    borderRadius: theme.borders.buttonRadius,
+    marginTop: 20,
+    flexGrow: 1,
+    flexShrink: 1,
+    marginLeft: 10,
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
+
+const ReviewContainer = ({ review }) => {
+  const navigate = useNavigate();
+
+  const handleLinkPress = () => {
+    navigate(`/repository/${review.repository.id}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Review review={review} />
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.buttonLeft} onPress={handleLinkPress}>
+          <Text style={styles.buttonText}>View repository</Text>
+        </Pressable>
+        <Pressable
+          style={styles.buttonRight}
+          onPress={() => console.log("pressed delete")}
+        >
+          <Text style={styles.buttonText}>Delete review</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
 const MyReviews = () => {
   const user = useMe(true).me;
@@ -35,7 +94,7 @@ const MyReviews = () => {
     <View style={styles.container}>
       <FlatList
         data={reviews}
-        renderItem={({ item }) => <Review review={item} />}
+        renderItem={({ item }) => <ReviewContainer review={item} />}
         keyExtractor={({ id }) => id}
         ItemSeparatorComponent={ItemSeparator}
       />
